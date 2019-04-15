5c3f148db79178732b2eeb61f60af5c2ff6510ca
selineTpy <1178274507@qq.com>
2019-04-15 20:49:25 +0800

    公共的js

--- public/template.js
+++ public/template.js
@@ -19,7 +19,9 @@ Date.prototype.Format = function(fmt) {
             fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
     return fmt;
   };
-
+/**
+ * 正则格式替换
+ */
   template.helper("format", function(jsonStr, value) {
     console.log(jsonStr,value)
     var obj = JSON.parse(jsonStr);
